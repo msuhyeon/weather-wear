72ef77ec30f32d9b48b010408b49250694f941e1
msuhyeon <job.maengsh@gmail.com>
2024-09-18 23:59:18 +0900

    Feat: layout 컴포넌트 분리

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,6 @@ import WeatherInfo from "@/components/WeatherInfo/WeatherInfo";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <header className={styles.header}>
-        <h1>WeatherWear</h1>
-      </header>
       <main className={styles.content}>
         <WeatherInfo />
         <Link href="/recommendation" className={styles.recommendButton}>

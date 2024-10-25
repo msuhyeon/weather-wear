75fc8f653226162733e2071c13d94fd73d047849
msuhyeon <job.maengsh@gmail.com>
2024-10-25 21:56:26 +0900

    Feat: skeleton UI 모듈 변경, delay 추가

--- components/weatherInfo/WeatherInfo.tsx
+++ components/weatherInfo/WeatherInfo.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import styles from "./styles.module.css";
 import { useQuery } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 interface WeatherCondition {
   icon: string;
@@ -28,15 +30,41 @@ const WeatherDisplay = ({
 }: {
   coordinates: { lat: number; lon: number };
 }) => {
-  const { data: weatherData } = useQuery<WeatherData>({
+  const {
+    data: weatherData,
+    error,
+    isError,
+  } = useQuery<WeatherData>({
     queryKey: ["weather", coordinates],
     queryFn: () => fetchWeatherData(coordinates),
-    suspense: true,
     enabled: coordinates.lat !== 0 && coordinates.lon !== 0,
+    retry: 2,
   });
 
   if (!weatherData || !weatherData.current) {
-    return <div>날씨 정보를 찾을 수 없습니다.</div>;
+    return (
+      <div className={styles.weatherCard}>
+        <Skeleton height={130} width={130} />
+        <div className={styles.weatherInfoWrap}>
+          <h2 className={styles.title}>
+            <Skeleton height={20} />
+          </h2>
+          <p className={styles.currentTemp}>
+            <Skeleton height={30} />
+          </p>
+          <p className={styles.info}>
+            <Skeleton height={16} />
+          </p>
+          <p className={styles.info}>
+            <Skeleton height={16} />
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <div>날씨 정보를 찾을 수 없습니다. {error.message}</div>;
   }
 
   return (

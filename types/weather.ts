62afecbeb39bf1935138311c7b95172d6b72cff8
msuhyeon <job.maengsh@gmail.com>
2024-11-01 23:22:30 +0900

    Chore: 타입 파일 위치 변경

---
+++ types/weather.ts
@@ -0,0 +1,27 @@
+export interface WeatherCondition {
+  icon: string;
+  description: string;
+}
+
+export interface CurrentData {
+  temp: number;
+  humidity: number;
+  weather: WeatherCondition[];
+  feels_like: number;
+}
+
+export interface WeatherData {
+  current: CurrentData | null;
+  error: Object | null;
+  isError: boolean;
+}
+
+type Gender = "male" | "female";
+
+export interface RecommendationData {
+  gender: Gender;
+  currentTemperature: number;
+  min_temp: number;
+  max_temp: number;
+  clothing: Record<Gender, string[]>;
+}

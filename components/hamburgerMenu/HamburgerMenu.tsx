4ba7e33c6cc44e1690b11c659664ab710e53d450
msuhyeon <job.maengsh@gmail.com>
2025-01-14 18:29:51 +0900

    feat: 햄버거 메뉴 ui 수정 (WIP)

--- components/hamburgerMenu/HamburgerMenu.tsx
+++ components/hamburgerMenu/HamburgerMenu.tsx
@@ -3,11 +3,13 @@
 import { useState, useEffect, useRef } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
+import CancelIcon from "@mui/icons-material/Cancel";
 import styles from "./styles.module.css";
 
 export default function HamburgerMenu() {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const navRef = useRef<HTMLElement>(null);
+  const [cityInput, setCityInput] = useState("");
 
   const handleHamburgerClick = (): void => {
     setIsNavOpen(!isNavOpen);
@@ -30,6 +32,18 @@ export default function HamburgerMenu() {
     };
   }, [isNavOpen]);
 
+  const handleChange = (e) => {
+    setCityInput(e.target.value);
+  };
+
+  const handleSearch = () => {
+    // 검색 api 호출
+  };
+
+  const handleReset = () => {
+    setCityInput("");
+  };
+
   return (
     <>
       <div onClick={handleHamburgerClick} className={styles.hamburgerBtn}>
@@ -39,12 +53,23 @@ export default function HamburgerMenu() {
         ref={navRef}
         className={`${styles.sideNav} ${isNavOpen ? styles.open : ""}`}
       >
-        <button className={styles.searchButton}>
-          <SearchIcon />
-        </button>
+        <fieldset className={styles.innerForm}>
+          <input
+            type="text"
+            className={styles.inputForm}
+            placeholder="어느 도시의 날씨가 궁금한가요?"
+            title="도시 이름 검색  "
+            onChange={handleChange}
+          />
+          <button className={styles.resetButton} onClick={handleReset}>
+            <CancelIcon />
+          </button>
+          <button className={styles.searchButton} onClick={handleSearch}>
+            <SearchIcon />
+          </button>
+        </fieldset>
         <ul className={styles.locationList}>
           <li className={styles.location}>
-            추가된 지역
             <span>location</span>
             <span>
               <i>icon</i> temperature

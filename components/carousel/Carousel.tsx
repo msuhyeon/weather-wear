72e8907984e1516c1747935f5c4757e24e3bbaaf
msuhyeon <job.maengsh@gmail.com>
2025-02-17 01:46:08 +0900

    refactor: swiper 수정

--- components/carousel/Carousel.tsx
+++ components/carousel/Carousel.tsx
@@ -1,10 +1,12 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useRef, useState } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
-import { Swiper as SwiperType } from "swiper";
+import type { SwiperRef } from "swiper/react";
 import styles from "./styles.module.css";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 interface RecommendationItem {
   category: string;
@@ -17,46 +19,50 @@ interface CarouselProps {
 }
 
 const Carousel: React.FC<CarouselProps> = ({ slides }) => {
-  const swiperRef = useRef<SwiperType | null>(null); // 메인 Swiper 참조
+  const swiperRef = useRef<SwiperRef | null>(null);
   const prevRef = useRef<HTMLButtonElement | null>(null);
   const nextRef = useRef<HTMLButtonElement | null>(null);
-
-  useEffect(() => {
-    if (swiperRef.current) {
-      swiperRef.current.params.navigation = {
-        nextEl: nextRef.current,
-        prevEl: prevRef.current,
-      };
-      swiperRef.current.navigation.init();
-      swiperRef.current.navigation.update();
-    }
-  }, []);
+  const [activeIndex, setActiveIndex] = useState<number>(0);
 
   return (
     <div className={styles.swiperWrapper}>
       <Swiper
         ref={swiperRef}
         modules={[Navigation, Pagination]}
-        spaceBetween={20}
-        slidesPerView={1}
+        spaceBetween={10}
+        slidesPerView={"auto"}
+        centeredSlides={true}
+        loop={true}
         pagination={{ clickable: true }}
-        loop
-        onSwiper={(swiper) => (swiperRef.current = swiper)}
+        navigation={{ nextEl: prevRef.current, prevEl: nextRef.current }}
+        onSwiper={(swiper) => {
+          if (swiperRef.current) {
+            swiperRef.current.swiper = swiper;
+          }
+          setActiveIndex(swiper.realIndex);
+        }}
+        onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
       >
         {slides.map((item, index) => (
-          <SwiperSlide className={styles.card} key={index}>
+          <SwiperSlide
+            className={`${styles.card} ${
+              activeIndex === index ? styles.active : styles.inactive
+            }`}
+            key={index}
+          >
             <span className={styles.category}>{item.category}</span>
-            <div>{item.imageKey}</div>
+            {/* TODO: imageKey는 이미지로 변경 예정 */}
+            <div className={styles.imageKey}>{item.imageKey}</div>
             <p className={styles.desc}>{item.description}</p>
           </SwiperSlide>
         ))}
       </Swiper>
       <div className={styles.swiperNavigation}>
         <button ref={prevRef} className={styles.swiperButtonPrev}>
-          Prev
+          <KeyboardArrowLeftIcon />
         </button>
         <button ref={nextRef} className={styles.swiperButtonNext}>
-          Next
+          <KeyboardArrowRightIcon />
         </button>
       </div>
     </div>
